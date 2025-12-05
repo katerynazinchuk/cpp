12e067f9f502e7bc757b8d651eb9119ff741cbf2
Kateryna Zinchuk <zinchuck.k@gmail.com>
2025-12-05 17:52:01 +0100

    add Makefile for megaphone and PhoneBook

--- rank_04/cpp_00/ex00/megaphone.cpp
+++ rank_04/cpp_00/ex00/megaphone.cpp
@@ -8,14 +8,14 @@ int main(int argc, char * argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 		return 1;
 	}
-	
 	for(int j = 1; j < argc; j++)
 	{
 		std::string arg = argv[j];
-		for(int i = 0; i < arg.size(); i++)
+		for(std::size_t i = 0; i < arg.size(); i++)
 		{
 			std::cout << (char)std::toupper(arg[i]);
 		}
+		std::cout << " ";
 	}
 		std::cout << std::endl;
 	return 0;
