d1ca1528962ffcf94f2003991a107a463eb753e4
Kateryna Zinchuk <zinchuck.k@gmail.com>
2026-01-21 13:28:33 +0100

    cpp_01 ex03

---
+++ rank_04/cpp_01/ex03/HumanA.hpp
@@ -0,0 +1,18 @@
+#ifndef HUMAN_A_HHP
+# define HUMAN_A_HHP
+
+#include <string>
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanA
+{
+    private:
+        std::string _name;
+        Weapon& _weapon;
+    public:
+        HumanA(std::string name, Weapon& weapon);
+        void attack(void) const;
+}
+
+#endif
---
+++ rank_04/cpp_01/ex03/main.cpp
@@ -0,0 +1,10 @@
+#include "Weapon.hpp"
+
+int main (void)
+{
+    Weapon club("crude spiked club");
+    std::cout << club.getType() << std::endl;
+    club.setType("some other type of club");
+    std::cout <<club.getType() << std::endl;
+    return (0);
+}
