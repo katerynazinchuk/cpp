1b841173c73c46703f6ed791ce8b5a1f4585f9e2
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-04 14:44:56 +0200

    Add signForm method to Bureaucrat.cpp

--- rank_05/cpp_05/ex01/Bureaucrat.cpp
+++ rank_05/cpp_05/ex01/Bureaucrat.cpp
@@ -1,4 +1,5 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 Bureaucrat::Bureaucrat(const std::string& name, int grade)
     : _name(name), _grade(grade)
@@ -59,6 +60,20 @@ const char* Bureaucrat::GradeTooHighException::what() const throw()
 const char* Bureaucrat::GradeTooLowException::what() const throw()
 {
     return "Grade is too low!";
+}
+
+void Bureaucrat::signForm(Form& f)
+{
+    try
+    {
+        f.beSigned(*this);
+        std::cout << _name << " signed " << f.getName() << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << _name << " couldn't sign " << f.getName()
+                  << " because " << e.what() << std::endl;
+    }
 }
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& b)
