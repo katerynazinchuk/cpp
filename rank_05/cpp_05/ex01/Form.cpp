76e4d4b34f65b7fec075df39b19e62c59bd97ea8
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-04 14:11:59 +0200

    Add getters and beSigned to Form.cpp

--- rank_05/cpp_05/ex01/Form.cpp
+++ rank_05/cpp_05/ex01/Form.cpp
@@ -1,4 +1,4 @@
-#include "Form.hpp"
+b#include "Form.hpp"
 #include "Bureaucrat.hpp"
 
 Form::Form(const std::string& name, int gradeToSign, int gradeToExec)
@@ -28,3 +28,50 @@ Form& Form::operator=(const Form& other)
 Form::~Form()
 {
 }
+
+const std::string& Form::getName() const
+{
+    return _name;
+}
+
+bool Form::getIsSigned() const
+{
+    return _isSigned;
+}
+
+int Form::getGradeToSign() const
+{
+    return _gradeToSign;
+}
+
+int Form::getGradeToExec() const
+{
+    return _gradeToExec;
+}
+
+void Form::beSigned(const Bureaucrat& b)
+{
+    if (b.getGrade() > _gradeToSign)
+        throw GradeTooLowException();
+    _isSigned = true;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high!";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low!";
+}
+
+std::ostream& operator<<(std::ostream& os, const Form& f)
+{
+    os << "Form: " << f.getName()
+       << ", signed: " << (f.getIsSigned() ? "yes" : "no")
+       << ", grade to sign: " << f.getGradeToSign()
+       << ", grade to exec: " << f.getGradeToExec();
+    return os;
+}
+
