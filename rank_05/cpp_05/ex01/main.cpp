eb6913ec05de3df6e37960089ddceb146d94b00f
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-04 14:51:20 +0200

    Add tests for valid and invalid form signs

--- rank_05/cpp_05/ex01/main.cpp
+++ rank_05/cpp_05/ex01/main.cpp
@@ -1,56 +1,73 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main()
 {
-	std::cout << "===Valid bureaucrat===" << std::endl;
-	try
-	{
-		Bureaucrat b("B", 5);
-		std::cout << b << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << "===Grade exceptions===" << std::endl;
-	try
-	{
-		Bureaucrat b("B", 0);
-		std::cout << b << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	try
-	{
-		Bureaucrat b("B", 151);
-		std::cout << b << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << "===Increment===" << std::endl;
-	try
-	{
-		Bureaucrat b("B", 1);
-		std::cout << b << std::endl;
-		b.incrementGrade();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	std::cout << "===Decrement===" << std::endl;
-	try
-	{
-		Bureaucrat b("B", 150);
-		std::cout << b << std::endl;
-		b.decrementGrade();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+    std::cout << "=== Valid form ===" << std::endl;
+    try
+    {
+        Form f("Budget", 50, 25);
+        std::cout << f << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "=== Invalid form grade ===" << std::endl;
+    try
+    {
+        Form f("Tax", 0, 25);
+        std::cout << f << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "=== Bureaucrat can sign ===" << std::endl;
+    try
+    {
+        Bureaucrat alice("Alice", 10);
+        Form f("Budget", 50, 25);
+        std::cout << alice << std::endl;
+        std::cout << f << std::endl;
+        alice.signForm(f);
+        std::cout << f << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "=== Bureaucrat cannot sign ===" << std::endl;
+    try
+    {
+        Bureaucrat bob("Bob", 140);
+        Form f("Budget", 50, 25);
+        std::cout << bob << std::endl;
+        std::cout << f << std::endl;
+        bob.signForm(f);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "=== Sign form twice ===" << std::endl;
+    try
+    {
+        Bureaucrat alice("Alice", 10);
+        Form f("Budget", 50, 25);
+        alice.signForm(f);
+        std::cout << f << std::endl;
+        alice.signForm(f);
+        std::cout << f << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
 }
