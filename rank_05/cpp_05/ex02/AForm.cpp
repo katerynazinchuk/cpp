71c55bc267c4eb7cc175eb78e6c12c137da856ad
Kateryna Zinchuk <zinchuck.k@gmail.com>
2026-08-06 12:47:45 +0200

    add structure files to ex02 ex03 cpp_05

--- rank_05/cpp_05/ex02/AForm.cpp
+++ rank_05/cpp_05/ex02/AForm.cpp
@@ -1,1 +1,77 @@
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+
+Form::Form(const std::string& name, int gradeToSign, int gradeToExec)
+    : _name(name), _isSigned(false), _gradeToSign(gradeToSign), _gradeToExec(gradeToExec)
+{
+    if (gradeToSign < 1 || gradeToExec < 1)
+        throw GradeTooHighException();
+    if (gradeToSign > 150 || gradeToExec > 150)
+        throw GradeTooLowException();
+}
+
+Form::Form(const Form& other)
+    : _name(other._name),
+      _isSigned(other._isSigned),
+      _gradeToSign(other._gradeToSign),
+      _gradeToExec(other._gradeToExec)
+{
+}
+
+Form& Form::operator=(const Form& other)
+{
+    if (this != &other)
+        _isSigned = other._isSigned;
+    return *this;
+}
+
+Form::~Form()
+{
+}
+
+const std::string& Form::getName() const
+{
+    return _name;
+}
+
+bool Form::getIsSigned() const
+{
+    return _isSigned;
+}
+
+int Form::getGradeToSign() const
+{
+    return _gradeToSign;
+}
+
+int Form::getGradeToExec() const
+{
+    return _gradeToExec;
+}
+
+void Form::beSigned(const Bureaucrat& b)
+{
+    if (b.getGrade() > _gradeToSign)
+        throw GradeTooLowException();
+    _isSigned = true;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high!";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low!";
+}
+
+std::ostream& operator<<(std::ostream& os, const Form& f)
+{
+    os << "Form: " << f.getName()
+       << ", signed: " << (f.getIsSigned() ? "yes" : "no")
+       << ", grade to sign: " << f.getGradeToSign()
+       << ", grade to exec: " << f.getGradeToExec();
+    return os;
+}
 
--- rank_05/cpp_05/ex02/AForm.hpp
+++ rank_05/cpp_05/ex02/AForm.hpp
@@ -1,1 +1,46 @@
 
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat;
+
+class Form
+{
+private:
+    const std::string	_name;
+    bool				_isSigned;
+    const int			_gradeToSign;
+    const int			_gradeToExec;
+
+public:
+    Form(const std::string& name, int gradeToSign, int gradeToExec);
+    Form(const Form& other);
+    Form& operator=(const Form& other);
+    ~Form();
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        const char* what() const throw();
+    };
+
+    const std::string& getName() const;
+    bool getIsSigned() const;
+    int getGradeToSign() const;
+    int getGradeToExec() const;
+
+    void beSigned(const Bureaucrat& b);
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& f);
+
+#endif
