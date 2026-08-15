64fd63689f55475c4633588fed003aa627a71460
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-15 17:17:19 +0200

    Add orthodox canonical form to Span.cpp

--- rank_05/cpp_08/ex01/Span.cpp
+++ rank_05/cpp_08/ex01/Span.cpp
@@ -1,1 +1,28 @@
 #include "Span.hpp"
+
+Span::Span() : _n(0)
+{
+}
+
+Span::Span(unsigned int n) : _n(n)
+{
+	_numbers.reserve(n);
+}
+
+Span::Span(const Span& other) : _n(other._n), _numbers(other._numbers)
+{
+}
+
+Span& Span::operator=(const Span& other)
+{
+	if (this != &other)
+	{
+		_n = other._n;
+		_numbers = other._numbers;
+	}
+	return *this;
+}
+
+Span::~Span()
+{
+}
