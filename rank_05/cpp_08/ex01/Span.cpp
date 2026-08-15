a80eae19552747b585fc879448832d14df3270e9
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-15 19:57:57 +0200

    Add shortest and longest span logic and change return type in Span.cpp

--- rank_05/cpp_08/ex01/Span.cpp
+++ rank_05/cpp_08/ex01/Span.cpp
@@ -34,11 +34,31 @@ void Span::addNumber(int number)
 	_numbers.push_back(number);
 }
 
-int Span::longestSpan() const
+long Span::longestSpan() const
 {
+	if (_numbers.size() < 2)
+		throw std::runtime_error("not enough numbers to find a span");
+	const std::vector<int>::const_iterator minIt = std::min_element(_numbers.begin(), _numbers.end());
+	const std::vector<int>::const_iterator maxIt = std::max_element(_numbers.begin(), _numbers.end());
+	return static_cast<long>(*maxIt) - static_cast<long>(*minIt);
 }
 
-int Span::shortestSpan() const
-{
 
+long Span::shortestSpan() const
+{
+	if (_numbers.size() < 2)
+		throw std::runtime_error("not enough numbers to find a span");
+
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+
+	long shortest = LONG_MAX;
+	for (size_t i = 1; i < sorted.size(); ++i)
+	{
+		long diff = static_cast<long>(sorted[i]) - static_cast<long>(sorted[i - 1]);
+		if (diff < shortest)
+			shortest = diff;
+	}
+	return shortest;
 }
+
