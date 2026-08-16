3c04e73a0fa27c5eb5f2ad62c957bf70c6943d97
Kateryna Zinchuk <zinchuck.k@gmail.com>
2026-08-16 21:40:49 +0200

    add two test cases to ex02

--- rank_05/cpp_08/ex02/main.cpp
+++ rank_05/cpp_08/ex02/main.cpp
@@ -33,13 +33,44 @@ int main()
 	std::stack<int> s(mstack);
 
 	std::cout << "=== test with std::list ===" << std::endl;
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << lst.back() << std::endl;
+	lst.pop_back();
+	std::cout << lst.size() << std::endl;
 
-
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+	std::list<int>::iterator lit = lst.begin();
+	std::list<int>::iterator lite = lst.end();
+	++lit;
+	--lit;
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
 	std::cout << "=== empty stack ===" << std::endl;
-
+	MutantStack<int> empty;
+	std::cout << empty.size() << std::endl;
+	if (empty.begin() == empty.end())
+		std::cout << "begin == end, nothing to iterate" << std::endl;
 
 	std::cout << "=== copy constructor ===" << std::endl;
-
+	MutantStack<int> copy(mstack);
+	MutantStack<int>::iterator itt = copy.begin();
+	MutantStack<int>::iterator itte = copy.end();
+	while (itt != itte)
+	{
+		std::cout << *itt << std::endl;
+		++itt;
+	}
+	copy.push(999);
+	std::cout << mstack.size() << std::endl;
+	std::cout << copy.size() << std::endl;
 
 	std::cout << "=== assignment operator ===" << std::endl;
 
