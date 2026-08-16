a4352aac5ceb9378e47692deda465cb9ffd86e08
Kateryna Zinchuk <109545458+katerynazinchuk@users.noreply.github.com>
2026-08-16 11:40:17 +0200

    Add additional test names to main.cpp

--- rank_05/cpp_08/ex02/main.cpp
+++ rank_05/cpp_08/ex02/main.cpp
@@ -1,25 +1,51 @@
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+#include <string>
+
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	std::cout << "=== subject example ===" << std::endl;
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "=== test with std::list ===" << std::endl;
+
+
+	std::cout << "=== empty stack ===" << std::endl;
+
+
+	std::cout << "=== copy constructor ===" << std::endl;
+
+
+	std::cout << "=== assignment operator ===" << std::endl;
+
+
+	std::cout << "=== another type: std::string ===" << std::endl;
+
+
+	return 0;
 }
