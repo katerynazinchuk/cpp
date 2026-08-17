a2a7747e269ac068ffa89352da219b8cbbda8f9a
Kateryna Zinchuk <zinchuck.k@gmail.com>
2026-08-17 17:42:07 +0200

    upload data to ex00 cpp_09

--- rank_05/cpp_09/ex00/main.cpp
+++ rank_05/cpp_09/ex00/main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
+#include <map>
 
 int main ()
 {
+	std::map<std::string, double> rates;
 	std::string line;
 	std::ifstream file("data.csv");
 	if (!file.is_open()) 
@@ -12,7 +14,14 @@ int main ()
 	std::getline(file, line);
 	while (std::getline(file, line))
 	{
-		std::cout << line << std::endl;
+		size_t pos = line.find(',');
+		if(pos == std::string::npos)
+			continue;
+		std::string date = line.substr(0, pos);
+		std::string rate = line.substr(pos + 1);
+		rates[date] = std::stod(rate);
+		// std::cout << "[" << date << "]" << "[" << rate << "]" << std::endl;
 	}
+	std::cout << rates.size() << std::endl;
 	return 0;
 }
